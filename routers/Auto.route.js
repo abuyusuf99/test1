2ab90e93e19c3dccf6d6de0e32e5c525d223135b
lom-ali <lomali1999999@mail.ru>
2024-05-06 16:06:05 +0300

    вывод авто по бренду

--- routers/Auto.route.js
+++ routers/Auto.route.js
@@ -7,4 +7,5 @@ router.get("/auto", autoControllers.getAuto);
 router.delete("/auto/:id", autoControllers.deleteAuto);
 router.get("/auto/:id", autoControllers.getOneAuto);
 router.patch("/auto/:id", autoControllers.patchAuto);
+router.get("/auto/brand/:id", autoControllers.getAutoByBrand)
 module.exports = router;
